ba3ba8b4379d40787ff29467c6918b721d97d173
NeilH <neilhemm0@gmail.com>
2023-01-07 15:28:13 -0500

    Add form for adding books

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Javascript for TOP-library
+
+let myLibrary = [];
+
+function Book(title, author, pages, genre, progress) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.genre = genre;
+    this.progress = progress;
+}
+
+function addBookToLibrary() {
+    
+}
