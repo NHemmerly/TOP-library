f6418148d9babdd5cbca554e6753ef90c0530a0b
NeilH <neilhemm0@gmail.com>
2023-01-07 19:35:00 -0500

    Add a dimmer whenever form is open

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Javascript for TOP-library
 
+const addBookForm = document.querySelector(".add-book-form");
+const cancelBookForm = document.getElementById("cancel");
+const dim = document.querySelector(".frost");
+
 let myLibrary = [];
 
 function Book(title, author, pages, genre, progress) {
@@ -11,3 +15,17 @@ function Book(title, author, pages, genre, progress) {
 }
 
 function addBookToLibrary() {}
+
+function openForm() {
+    addBookForm.style.display = 'block';
+    dim.style.display = 'block';
+}
+
+function closeForm() {
+    addBookForm.style.display = 'none';
+    dim.style.display = 'none';
+}
+
+const addBook = document.querySelector(".add-book");
+addBook.addEventListener('click', openForm);
+cancelBookForm.addEventListener('click', closeForm);
